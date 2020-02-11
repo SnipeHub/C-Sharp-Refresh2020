972339ce20ba344f87f9d51a62d2c1c356aea007
SnipeHub <modmapper@hotmail.com>
2020-02-11 21:14:33 +1100

    If and functions

--- MultipleChoiceQuestions/MultipleChoiceQuestions/Program.cs
+++ MultipleChoiceQuestions/MultipleChoiceQuestions/Program.cs
@@ -11,23 +11,42 @@ namespace MultipleChoiceQuestions
         public static void Main(string[] args)
         {
             //string Readback;
-
             Console.WriteLine("What is your name? ");
             var name = Console.ReadLine();
             name = name.ToLower();
 
+            if (name == "")
+            {
+                NothingEntered();
+                name = Console.ReadLine();
+                name = name.ToLower();
+            }
+
             Console.WriteLine("How old are you? ");
             var age = Console.ReadLine();
+
+            if (age == "")
+            {
+                NothingEntered();
+                age = Console.ReadLine();
+            }
 
             Console.WriteLine("What month were you born in? ");
             var born = Console.ReadLine();
             born = born.ToLower();
 
+            if (born == "")
+            {
+                NothingEntered();
+                born = Console.ReadLine();
+                born = born.ToLower();
+            }
+
             if (born != "more")
             {
                 Console.WriteLine("Hint: next time enter 'more' for the month!");
             }
-            
+
             // option output 1
             //entered info output
             //Readback = $"Your name is {name}, you are {age} years old and you were born in the month of {born}";
@@ -40,6 +59,9 @@ namespace MultipleChoiceQuestions
             // option output 2
             Console.WriteLine($"Your name is: {name}");
             Console.WriteLine($"Your age is: {age}");
+            //Console.WriteLine($"Your born in: {born}");
+
+
             if (born == "more")
             {
                 Console.WriteLine("You entered the secret hint!");
@@ -48,7 +70,7 @@ namespace MultipleChoiceQuestions
             {
                 Console.WriteLine($"Your born in: {born}");
             }
-            
+
 
             if (born == "november")
             {
@@ -62,7 +84,13 @@ namespace MultipleChoiceQuestions
             {
                 Console.WriteLine("Did you know that there are actually 14 constellations?");
             }
-        
+
+
+        }
+
+        static void NothingEntered()
+        {
+            Console.WriteLine("You did not enter anything, please try again");
         }
     }
 }
---
+++ TrackerForeachLoop/TrackerForeachLoop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var studentGrades = new int[] { 80, 77, 45, 87, 53, 68, 93, 98, 87, 95, 87 };
+
+            foreach (var studentGrade in studentGrades)
+            {
+                Console.WriteLine(studentGrade);
+            }
+            
+        }
+    }
+}
