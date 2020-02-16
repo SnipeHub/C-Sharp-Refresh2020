5eba7a41c6a1821a97cc686ec594354b50f45da7
SnipeHub <modmapper@hotmail.com>
2020-02-16 15:13:51 +1100

    Challenge- arrays

---
+++ JobCandidate/JobCandidate/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobCandidate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many years of experience do you have?");
+
+            var years = int.Parse(Console.ReadLine());
+
+            switch (years)
+            {
+                case 0:
+                    Console.WriteLine("Inexperienced");
+                    break;
+                case 1:
+                    Console.WriteLine("Junior");
+                    break;
+                case 2:
+                    Console.WriteLine("Intermediate");
+                    break;
+                case 3:
+                    Console.WriteLine("Advanced");
+                    break;
+                default:    // default is used like an else
+                    Console.WriteLine("Senior");
+                    break;
+            }
+
+        }
+    }
+}
