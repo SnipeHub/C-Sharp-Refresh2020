7ae2aa1abcd1571719951d540679e07223b28a93
SnipeHub <modmapper@hotmail.com>
2020-02-23 11:32:21 +1100

    Encapsulation
    - Made a variable private and linked it to a method

---
+++ StudentTracker-Encapsulation/StudentTracker-Encapsulation/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudentTrackerImproved
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var students = new List<Student>();
+
+            var adding = true;
+
+            while (adding)
+            {
+                var newStudent = new Student();
+
+                Console.Write("Student Name: ");
+                newStudent.Name = Console.ReadLine();
+
+                Console.Write("Student Grade: ");
+                newStudent.Grade = int.Parse(Console.ReadLine());
+
+                Console.Write("Student Birthday: ");
+                newStudent.Birthday = Console.ReadLine();
+
+                Console.Write("Student Address: ");
+                newStudent.Address = Console.ReadLine();
+
+                Console.Write("Student Phone: ");
+                newStudent.SetPhone (int.Parse(Console.ReadLine()));
+
+                students.Add(newStudent);
+
+                Console.WriteLine("Add another? y/n");
+
+                if (Console.ReadLine() != "y")
+                {
+                    adding = false;
+                }
+            }
+
+            foreach (var student in students)
+            {
+                Console.WriteLine("Name: {0}, Grade: {1}, Birthday: {2}, Address: {3}", student.Name, student.Grade, student.Birthday, student.Address);
+            }
+        }
+    }
+
+    class Student
+    {
+        public string Name;
+        public int Grade;
+        public string Birthday;
+        public string Address;
+        private int phone;
+
+        public void SetPhone(int number)
+        {
+            phone = number;
+        }
+    }
+}
